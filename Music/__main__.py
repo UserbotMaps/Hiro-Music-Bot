eed33b354fa6a4fad138eadd649d39b47fd37d40
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-02-13 04:25:00 +0700

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -54,9 +54,9 @@ async def load_start():
     await app.send_message(LOG_GROUP_ID, "Bot Started")
     await client.send_message(LOG_GROUP_ID, "Assistant Started")
     #Kalau clone atau fork jangan di hapus ya tolol
-    await client.join_chat("NastySupportt")
-    await client.join_chat("NastyProject")
-    await client.join_chat("ahhsudahlahhh")
+    await client.join_chat("hiroshisupport")
+    await client.join_chat("bombleebas")
+    await client.join_chat("demonits")
     print("[INFO]: STARTED")
     
    
